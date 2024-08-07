13a3a3b4a94ca090cc2b214107d0b053e1d2b2fc
Carlos Florêncio <carlosmflorencio@gmail.com>
2024-08-07 15:58:20 +0100

    macos support

--- plugin.sh
+++ plugin.sh
@@ -2,7 +2,7 @@
 
 x=$(tmux capture-pane -p -S '-' -J -t !)
 PROMPT_PATTERN=${PROMPT_PATTERN:-" ] % "}
-result=$(echo "$x" | tac | sed -e "0,/$PROMPT_PATTERN/d" | sed "/$PROMPT_PATTERN/,\$d" | tac)
+result=$(echo "$x" | tac | gsed -e "0,/$PROMPT_PATTERN/d" | gsed "/$PROMPT_PATTERN/,\$d" | tac)
 
 EDITOR_CMD=${EDITOR_CMD:-"$EDITOR -"}
 
